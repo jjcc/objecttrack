3e7faf138f602885fbb07a2ff6aeba546f0804f3
jjcc <jjccforth@gmail.com>
2025-07-03 22:51:43 -0400

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  // Enable source maps for debugging
+  webpack(config, { dev }) {
+  if (dev) {
+    config.devtool = "source-map";
+  }
+  return config;
+  }
 };
 
 export default nextConfig;
