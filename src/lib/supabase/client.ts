191aa5b99b0b552060d52e327e3c2bf3693092e4
jjcc <jjccforth@gmail.com>
2026-04-12 15:09:27 -0400

    env variable name fix

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -5,7 +5,7 @@ import type { SupabaseClient } from "@supabase/supabase-js";
 export function createClient() {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_KEY!
   );
 }
 
